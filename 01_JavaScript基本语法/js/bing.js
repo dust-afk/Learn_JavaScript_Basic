81659d4e7fb5bd24cd91e91c5d01be2b40dbee83
dust-afk <1780214897@qq.com>
2025-11-07 20:25:09 +0800

    Add files via upload

---
+++ 01_JavaScript基本语法/js/bing.js
@@ -0,0 +1,6 @@
+// 获取元素
+var bingAEI = document.querySelector(".bing")
+// 监听元素的点击
+bingAEI.onclick = function() {
+  alert('helloword')
+}
---
+++ 01_JavaScript基本语法/js/文档注释.js
@@ -0,0 +1,9 @@
+/**
+ * 和某人打招呼的函数
+ * @param {string} name 姓名
+ * @param {number} age 姓名
+ */
+function sayHello(name,age) {
+
+}
+sayHello("why",18)
